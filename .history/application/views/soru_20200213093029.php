0e3ec260cadc141418732d377302899790be5cd5
ogulcanpabuccu <59419630+ogulcanpabuccu@users.noreply.github.com>
2020-02-13 17:47:01 +0300

    agaa

---
+++ .history/application/views/soru_20200213093029.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">	
+	<link rel="stylesheet" type="text/css" href="/assets/themes/default/easyui.css">
+	<link rel="stylesheet" type="text/css" href="/assets/themes/icon.css">
+
+	<link rel="stylesheet" href="style.css">
+	<title>Soru Detayı</title>
+</head>
+
+<body>
+
+<div class="container">
+
+
+
+		<div class="row mt-3">
+
+
+			<label> Soran Kişi </label><input class="easyui-textbox" readonly style="width:300px" value="<?php echo $sorudetay->soran ?>">
+			<label> Kime Soruyor </label><input class="easyui-textbox" readonly  style="width:300px" value="<?php echo $sorudetay->alici ?>">
+		</div>
+		<div class="row mt-3">
+		
+		<label>Sorulduğu Zaman</label><input class="easyui-textbox" readonly  style="width:300px" value="<?php echo $sorudetay->soruldugu_zaman ?>">
+		<label>Soru Bitiş Zamanı</label><input class="easyui-textbox" readonly style="width:300px" value="<?php echo $sorudetay->soru_zaman ?>">
+		</div>
+		<div class="row mt-3">
+		<label> Önem Derecesi </label><input class="easyui-textbox" readonly style="width:300px" value="<?php echo $sorudetay->onem ?>">
+		<label>Konu</label><input class="easyui-textbox" readonly style="width:300px" value="<?php echo $sorudetay->soru_konu ?>">
+		</div>
+		<div class="row mt-3">
+		<label class="mr-2" >Soru Detay:</label><?php echo $sorudetay->soru_detay ?>
+		</div>
+	</div>
+	<script type="text/javascript" src="/assets/jquery.min.js"></script>
+	<script type="text/javascript" src="/assets/jquery.easyui.min.js"></script>
+</body>
+
+</html>
---
+++ .history/application/views/soru_20200213093058.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+	<link rel="stylesheet" href="style.css">
+	<title>Soru Detayı</title>
+</head>
+
+<body>
+
+<div class="container">
+
+
+
+		<div class="row mt-3">
+
+
+			<label> Soran Kişi </label><input class="easyui-textbox" readonly style="width:300px" value="<?php echo $sorudetay->soran ?>">
+			<label> Kime Soruyor </label><input class="easyui-textbox" readonly  style="width:300px" value="<?php echo $sorudetay->alici ?>">
+		</div>
+		<div class="row mt-3">
+		
+		<label>Sorulduğu Zaman</label><input class="easyui-textbox" readonly  style="width:300px" value="<?php echo $sorudetay->soruldugu_zaman ?>">
+		<label>Soru Bitiş Zamanı</label><input class="easyui-textbox" readonly style="width:300px" value="<?php echo $sorudetay->soru_zaman ?>">
+		</div>
+		<div class="row mt-3">
+		<label> Önem Derecesi </label><input class="easyui-textbox" readonly style="width:300px" value="<?php echo $sorudetay->onem ?>">
+		<label>Konu</label><input class="easyui-textbox" readonly style="width:300px" value="<?php echo $sorudetay->soru_konu ?>">
+		</div>
+		<div class="row mt-3">
+		<label class="mr-2" >Soru Detay:</label><?php echo $sorudetay->soru_detay ?>
+		</div>
+	</div>
+
+</body>
+
+</html>
