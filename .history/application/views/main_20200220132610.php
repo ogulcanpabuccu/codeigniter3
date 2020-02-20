3935bbeeb88962c816607df55b60f63ed7350289
ogulcanpabuccu <59419630+ogulcanpabuccu@users.noreply.github.com>
2020-02-20 17:50:48 +0300

    updatee

---
+++ .history/application/views/main_20200220132610.php
@@ -0,0 +1,322 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+
+
+
+	<link rel="stylesheet" type="text/css" href="/assets/themes/default/easyui.css">
+	<link rel="stylesheet" type="text/css" href="/assets/themes/icon.css">
+	<link rel="stylesheet" type="text/css" href="/assets/dropzone.css">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+
+	<title>Hoşgeldiniz</title>
+
+
+</head>
+
+<body>
+
+	<div style="margin-left: 350px;" class="container mt-3">
+
+
+
+		<div id="p" class="easyui-panel" title="Panel" style="width:1300px;height:800px;padding:10px;background:#fafafa;" data-options="iconCls:'icon-save',closable:true,
+                collapsible:true,minimizable:true,maximizable:true">
+
+			<div id="tt" class="easyui-tabs" style="width:1250px;height:740px;">
+
+				<div title="Giriş" style="padding:20px;display:none;">
+
+					<?php
+
+					if (!isset($this->session->kullanici_mail)) { ?>
+
+
+						<form action="/Kullanici/giris" method="POST">
+							<div>
+								<label for="name">Email:</label>
+								<input class="easyui-textbox" prompt="Mail Adresini gir" name="email" required="required" data-options="validType:'email', required:true " />
+							</div>
+							<div>
+								<label for="email">Şifre:</label>
+								<input class="easyui-passwordbox" prompt="Şifreni gir" required="required" name="sifre" data-options=" required:true" />
+							</div>
+							<button id="btn" type="submit" class="easyui-linkbutton">Giriş</button>
+
+				</div>
+
+				</form>
+			<?php 	} else { ?>
+
+				Hoşgeldin <?php echo $this->session->kullanici_adsoyad ?>
+				<hr>
+				Çıkış Yapmak için tıklayın
+
+				<form action="/Kullanici/cikis" method="POST">
+
+
+					<button id="btncik" type="submit" class="easyui-linkbutton">Çıkış</button>
+
+				</form>
+
+
+
+			<?php	} ?>
+
+
+			</div>
+
+			<div title="Soru Sor" selected ; data-options="closable:true" style="overflow:auto;padding:20px;display:none;">
+
+				<form action="" method="POST" onsubmit="return false;" enctype="multipart/form-data" id="FormID1">
+					<div class="row">
+
+
+						<div class="col-md-6">
+							<label>Soran : </label> <input class="easyui-textbox" readonly style="width:150px" name="soran" value="<?php echo $this->session->kullanici_adsoyad ?>">
+						</div>
+
+
+
+
+						<div id="dosyalar"></div>
+
+
+					</div>
+					<div class="row">
+
+
+						<div class="col-md-6" style="margin-top:10px;">
+
+							<label>Alıcı Seç : </label> <select id="alici" name="alici" class="easyui-combobox" style="width:100px;">
+								<option value="0">Kullanıcı Seçiniz</option>
+								<? foreach ($kullanicilar as $kullanici) { ?>
+
+									<? if ($kullanici->kullanici_id != $this->session->kullanici_id) { ?>
+										<option value="<? echo $kullanici->kullanici_adsoyad; ?>"><? echo $kullanici->kullanici_adsoyad; ?></option>
+									<? } ?>
+
+								<? } ?>
+							</select>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-6" style="margin-top: 10px;">
+
+							<label>Önem Durumu :</label> <select id="onem" name="onem" class="easyui-combobox" style="width:100px;">
+								<option value="1">1</option>
+								<option value="2">2</option>
+								<option value="3">3</option>
+							</select>
+							<input class="easyui-datetimebox" name="day" id="day1" data-options="required:true,showSeconds:false" value="" prompt="Ay/Gün/Yıl Saat:Dakika" style="width:150px">
+
+						</div>
+
+					</div>
+
+					<div class="row">
+						<div class="col-md-6" style="margin-top: 10px;">
+
+							<label>Konu :</label> <input name="sorukonu" cols="56" rows="5" placeholder="Konu girin"></input>
+
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-6" style="margin-top: 10px;">
+
+							<textarea name="sorudetay" cols="56" rows="5" placeholder="Sorunuzu girin"></textarea>
+
+						</div>
+					</div>
+					<div class="row" style="margin-top:10px;">
+
+
+						<div id="some-dropzone" style="margin-left: 14px;" class="dropzone"></div>
+					</div>
+					<div class="row">
+						<button onclick="$.sorukaydet()" class="btn btn-success ml-3 mt-3" type="submit">Gönder</button>
+					</div>
+
+					<div id="sonuc"></div>
+			</div>
+			</form>
+
+			<div title="Sorular" data-options="closable:true" style="overflow:auto;padding:20px;display:none;">
+				<h1>Sorular</h1>
+				<table id="dg"> </table>
+
+			</div>
+
+
+		</div>
+
+	</div>
+	<footer>
+
+		<script type="text/javascript" src="/assets/jquery.min.js"></script>
+		<script type="text/javascript" src="/assets/jquery.easyui.min.js"></script>
+		<script type="text/javascript" src="/assets/dropzone.js"></script>
+
+		<script type="text/javascript">
+			Dropzone.options.someDropzone = {
+				url: "/file/upload",
+				success: function(data, gelenData) {
+					console.log(gelenData);
+					if (gelenData) {
+						$("#dosyalar").append('<input type="hidden" name="dosyalar[]" value="' + gelenData.resim_yol + '" />');
+					}
+				}
+			};
+		</script>
+
+		<script type="text/javascript">
+			$(function(sorudetay) {
+				$('#dg').datagrid({
+					url: '/kullanici/sorugonder',
+					width: 1070,
+					pagination: false,
+					pageNumber: 1,
+					pageSize: 10,
+					pageList: [10, 20, 30, 40, 50],
+					rownumbers: true,
+
+
+					ctrlSelect: true,
+					onClickRow(index, row) {
+						console.log('row', row);
+
+						$('#tt').tabs('add', {
+							id: 'sorutab' + row.id,
+							title: "#" + row.id + " - " + row.soru_konu,
+							closable: true,
+							href: '/kullanici/sorudetay/' + row.id,
+
+						});
+
+					},
+
+					columns: [
+						[{
+								field: 'soran',
+								title: 'Soran Kişi',
+								width: 120,
+							},
+							{
+								field: 'alici',
+								title: 'Kime Sordu',
+								width: 100,
+							},
+							{
+								field: 'onem',
+								title: 'Önemi',
+								width: 60,
+
+							},
+							{
+								field: 'soru_konu',
+								title: 'Konu',
+								width: 100,
+
+							},
+							{
+								field: 'soru_zaman',
+								title: 'İş Tamamlama Zamanı',
+								width: 150,
+
+							},
+							{
+								field: 'cevapsayi',
+								title: 'Cevaplanma Sayısı',
+								width: 130,
+
+							},
+							{
+								field: 'cevaplayan',
+								title: 'Cevapladın mı ?',
+								width: 110,
+								formatter: function(value) {
+									if (value > 0) {
+
+										return 'Evet (' + value + ')';
+
+									} else {
+										return 'Hayır';
+									}
+								}
+							},
+							{
+								field: 'dosyasayi',
+								title: 'Dosya Sayısı',
+								width: 100,
+
+							},
+							{
+								field: 'soncevapzamani',
+								title: 'Son Cevaplanma Zamanı',
+								width: 190,
+
+							}
+						]
+					]
+				});
+			});
+		</script>
+
+		<script type="text/javascript">
+			$(function() {
+				//<a href="/kullanici/sorudetay/' + gelenveri.soruId + '">soruya git</a>
+				$.sorukaydet = function() {
+					$.ajax({
+						url: '/kullanici/kaydet',
+						type: 'POST',
+						dataType: 'json',
+						data: $('#FormID1').serialize(),
+						success: function(gelenveri) {
+							if (gelenveri.success) {
+								$("#sonuc").html('Soru Başarıyla eklendi.');
+
+							} else {
+								$("#sonuc").html(gelenveri.hataMesaji);
+							}
+						}
+					});
+				}
+
+
+
+				$.cevapkaydet = function(soruId) {
+					$.ajax({
+						url: '/kullanici/cevapkaydet/' + soruId,
+						type: 'POST',
+						dataType: 'json',
+						refresh: true,
+						data: $('#Formcevap' + soruId).serialize(),
+						success: function(gelenveri) {
+							if (gelenveri.success) {
+								$("#cevapsonuc" + soruId).html(gelenveri.hataMesaji);
+								$("#cevapdetay" + soruId).val("");
+								var p = $('#tt').tabs('getTab', $('#tt').tabs('getTabIndex', $('#tt').tabs('getSelected')));
+								p.panel('refresh');
+							} else {
+								$("#cevapsonuc" + soruId).html(gelenveri.hataMesaji);
+							}
+						}
+					});
+				}
+
+			});
+		</script>
+
+
+
+	</footer>
+</body>
+
+</html>
--- todolist.sql
+++ todolist.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Anamakine: 127.0.0.1
--- Üretim Zamanı: 19 Şub 2020, 15:45:42
+-- Üretim Zamanı: 20 Şub 2020, 15:49:05
 -- Sunucu sürümü: 10.4.11-MariaDB
 -- PHP Sürümü: 7.4.2
 
@@ -55,7 +55,11 @@ INSERT INTO `cevap` (`id`, `soru_id`, `cevaplayan_id`, `cevap_detay`, `cevaplama
 (126, 69, 4, 'oldumu lütfeenn brom benim', '2020-02-19 14:11:55'),
 (127, 32, 1, 'merhaba kanka', '2020-02-19 14:16:27'),
 (128, 53, 2, 'dsadas', '2020-02-19 17:11:02'),
-(129, 53, 2, 'sdasa', '2020-02-19 17:11:12');
+(129, 53, 2, 'sdasa', '2020-02-19 17:11:12'),
+(130, 17, 1, 'yapma bee', '2020-02-20 09:16:00'),
+(131, 83, 1, 'hayret', '2020-02-20 09:33:03'),
+(132, 84, 1, 'adam', '2020-02-20 11:40:13'),
+(133, 53, 1, 'cevap', '2020-02-20 15:37:09');
 
 -- --------------------------------------------------------
 
@@ -69,15 +73,6 @@ CREATE TABLE `dosyalar` (
   `dosya_url` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Tablo döküm verisi `dosyalar`
---
-
-INSERT INTO `dosyalar` (`id`, `soru_id`, `dosya_url`) VALUES
-(1, 53, '/images/upload/indir_(1)14.jpg'),
-(2, 53, '/images/upload/indir6.jpg'),
-(3, 57, '/images/upload/indir7.jpg');
-
 -- --------------------------------------------------------
 
 --
@@ -87,6 +82,7 @@ INSERT INTO `dosyalar` (`id`, `soru_id`, `dosya_url`) VALUES
 CREATE TABLE `kullanici` (
   `kullanici_id` int(11) NOT NULL,
   `kullanici_mail` varchar(80) COLLATE utf8_turkish_ci NOT NULL,
+  `departman` varchar(200) COLLATE utf8_turkish_ci NOT NULL,
   `kullanici_adsoyad` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
   `kullanici_password` varchar(50) COLLATE utf8_turkish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
@@ -95,11 +91,11 @@ CREATE TABLE `kullanici` (
 -- Tablo döküm verisi `kullanici`
 --
 
-INSERT INTO `kullanici` (`kullanici_id`, `kullanici_mail`, `kullanici_adsoyad`, `kullanici_password`) VALUES
-(1, 'admin', 'Oğulcan Pabuccu', '123456'),
-(2, 'user', 'Ahmet Mehmet', '123456'),
-(3, 'kullanici1', 'Kadir İnanır', '123456'),
-(4, 'huseyin', 'Hüseyin Ulan', '123456');
+INSERT INTO `kullanici` (`kullanici_id`, `kullanici_mail`, `departman`, `kullanici_adsoyad`, `kullanici_password`) VALUES
+(1, 'admin', '2', 'Oğulcan Pabuccu', '123456'),
+(2, 'user', '2', 'Ahmet Mehmet', '123456'),
+(3, 'kullanici1', '1', 'Kadir İnanır', '123456'),
+(4, 'huseyin', '1', 'Hüseyin Ulan', '123456');
 
 -- --------------------------------------------------------
 
@@ -110,65 +106,25 @@ INSERT INTO `kullanici` (`kullanici_id`, `kullanici_mail`, `kullanici_adsoyad`, 
 CREATE TABLE `soru` (
   `id` int(11) NOT NULL,
   `soran` varchar(50) NOT NULL,
+  `departman` varchar(200) NOT NULL,
   `alici` varchar(50) NOT NULL,
   `onem` int(2) NOT NULL,
   `soru_zaman` varchar(500) DEFAULT NULL,
   `soruldugu_zaman` timestamp NOT NULL DEFAULT current_timestamp(),
   `soru_konu` varchar(200) NOT NULL,
-  `soru_detay` text NOT NULL
+  `soru_detay` text NOT NULL,
+  `sonuc` varchar(10) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Tablo döküm verisi `soru`
 --
 
-INSERT INTO `soru` (`id`, `soran`, `alici`, `onem`, `soru_zaman`, `soruldugu_zaman`, `soru_konu`, `soru_detay`) VALUES
-(17, 'admin', 'kullanici1', 1, '02/18/2020 17:34', '2020-02-11 14:34:58', 'dsa', 'dsaas'),
-(18, 'admin', 'kullanici1', 1, '02/18/2020 17:35', '2020-02-11 14:35:29', 'dsa', 'das'),
-(19, 'admin', 'huseyin', 1, '02/18/2020 17:43', '2020-02-11 14:43:25', 'konu', 'soru'),
-(20, 'admin', 'huseyin', 1, '02/18/2020 17:43', '2020-02-11 14:43:27', 'konu', 'soru'),
-(27, 'admin', 'kullanici1', 3, '02/26/2020 09:19', '2020-02-12 06:19:31', 'konuuu', 'hayat'),
-(28, 'admin', 'user', 3, '02/11/2020 13:22', '2020-02-12 10:22:39', 'konuuu', 'sorularrr'),
-(29, 'admin', 'kullanici1', 3, '02/18/2020 15:18', '2020-02-12 12:20:34', 'Oldumu Abi', 'Bence oldu abi alt tarafa da cevap alanını ekleyip cevap tablosu ile soru tablo id\'lerini eşleştirebilirsem tek sıkıntı görüntü olacak... '),
-(30, 'admin', 'user', 1, '02/18/2020 14:55', '2020-02-17 11:55:12', 'dsa', 'sadsa'),
-(32, 'admin', 'user', 3, '02/20/2020 14:29', '2020-02-18 11:29:42', 'rsim denem', 'resim olsunlütfen'),
-(33, 'admin', 'kullanici1', 3, '02/19/2020 14:30', '2020-02-18 11:30:49', 'dasdas', 'dsa'),
-(42, 'admin', 'kullanici1', 3, '02/19/2020 14:34', '2020-02-18 11:34:43', 'hahahahhah', 'ahahhahaha'),
-(43, 'admin', 'kullanici1', 1, '02/11/2020 15:24', '2020-02-18 12:25:01', 'Oldumu Abi', 'dasdas'),
-(46, 'admin', 'user', 2, '02/20/2020 15:25', '2020-02-18 12:25:36', 'dasdas', 'dsad'),
-(48, 'admin', 'user', 3, '02/19/2020 15:27', '2020-02-18 12:27:28', 'haydaaaaaa', 'aaaaaaaaaaa'),
-(49, 'admin', 'kullanici1', 2, '02/19/2020 16:06', '2020-02-18 13:06:35', 'Resim Deneme12223', 'dsadas'),
-(50, 'admin', 'user', 2, '02/19/2020 16:07', '2020-02-18 13:07:19', 'resim deneme123', 'aaaaaaaaa'),
-(51, 'admin', 'kullanici1', 1, '02/19/2020 16:20', '2020-02-18 13:20:25', 'olsun', 'artık'),
-(52, 'admin', 'user', 1, '02/25/2020 23:03', '2020-02-18 20:03:49', 'konuuu', 'oldu mu resim'),
-(53, 'admin', 'kullanici1', 1, '02/25/2020 23:13', '2020-02-18 20:13:28', 'resimm', 'asdasdasdasdsadafasd123413'),
-(54, 'admin', 'kullanici1', 1, '02/19/2020 23:32', '2020-02-18 20:32:08', 'adsdas', 'das'),
-(55, 'admin', 'kullanici1', 1, '02/19/2020 23:32', '2020-02-18 20:32:09', 'adsdas', 'das'),
-(56, 'admin', 'kullanici1', 1, '02/19/2020 23:32', '2020-02-18 20:32:19', 'adsdas', 'das'),
-(57, 'admin', 'kullanici1', 1, '02/19/2020 23:32', '2020-02-18 20:33:51', 'adsdas', 'das'),
-(58, 'admin', 'kullanici1', 1, '02/26/2020 23:35', '2020-02-18 20:35:21', 'asda', 'as'),
-(59, 'admin', 'kullanici1', 1, '02/26/2020 23:35', '2020-02-18 20:35:29', 'asda', 'as'),
-(60, 'admin', 'user', 1, '02/19/2020 23:40', '2020-02-18 20:40:40', 'dasdas', 'dasdas'),
-(61, 'admin', 'user', 3, '02/19/2020 23:41', '2020-02-18 20:41:14', 'aaaaaaaaaa', 'aaaaaaaaaaaasssssss'),
-(62, 'admin', 'kullanici1', 1, '02/12/2020 23:43', '2020-02-18 20:43:34', 'aaaaaaaaaa', 'aaaaaaaaaaaasssssss'),
-(63, 'admin', 'kullanici1', 1, '02/26/2020 23:44', '2020-02-18 20:44:41', 'aaaaaaa', 'sddsadas'),
-(64, 'admin', 'kullanici1', 1, '02/25/2020 23:51', '2020-02-18 20:51:51', 'dasda', 'dasdas'),
-(65, 'admin', 'kullanici1', 1, '02/19/2020 23:52', '2020-02-18 20:52:36', 'dsada', 'dasda'),
-(66, 'admin', 'kullanici1', 3, '02/12/2020 23:53', '2020-02-18 20:53:24', 'dsadas', 'dsada'),
-(67, 'admin', 'kullanici1', 1, '02/26/2020 00:06', '2020-02-18 21:06:30', 'dsada', 'dsadas'),
-(68, 'admin', 'huseyin', 1, '02/26/2020 00:07', '2020-02-18 21:07:24', 'dasd', 'das'),
-(69, 'admin', 'kullanici1', 1, '02/26/2020 00:07', '2020-02-18 21:08:01', 'dsa', 'dsa'),
-(70, 'user', 'huseyin', 1, '02/20/2020 17:13', '2020-02-19 14:13:42', 'dsafsdasdasdasfsadslkhfaslkşdkasşfjaslşdkawodşalmfkısjdmsşaldışawmşıfamdspaşdmşa', 'dsadas'),
-(71, 'user', 'admin', 1, '02/05/2020 17:16', '2020-02-19 14:16:13', 'flaksdlasşkdasnfalskjdlsakjdşaskşdlaskldaskşldskşşdlsakl', 'dsadas'),
-(72, 'user', 'admin', 1, '02/05/2020 17:16', '2020-02-19 14:16:14', 'flaksdlasşkdasnfalskjdlsakjdşaskşdlaskldaskşldskşşdlsakl', 'dsadas'),
-(73, 'user', 'admin', 1, '02/05/2020 17:16', '2020-02-19 14:16:15', 'flaksdlasşkdasnfalskjdlsakjdşaskşdlaskldaskşldskşşdlsakl', 'dsadas'),
-(74, 'user', 'admin', 1, '02/05/2020 17:16', '2020-02-19 14:16:15', 'flaksdlasşkdasnfalskjdlsakjdşaskşdlaskldaskşldskşşdlsakl', 'dsadas'),
-(75, 'user', 'admin', 1, '02/05/2020 17:16', '2020-02-19 14:16:15', 'flaksdlasşkdasnfalskjdlsakjdşaskşdlaskldaskşldskşşdlsakl', 'dsadas'),
-(76, 'user', 'admin', 1, '02/05/2020 17:16', '2020-02-19 14:16:15', 'flaksdlasşkdasnfalskjdlsakjdşaskşdlaskldaskşldskşşdlsakl', 'dsadas'),
-(77, 'user', 'admin', 1, '02/25/2020 17:17', '2020-02-19 14:17:18', 'ldsalkjdaşlsjdşlsakşldskaşldasdasdasfsaddasd', 'dsadasda'),
-(78, 'user', 'admin', 1, '02/25/2020 17:17', '2020-02-19 14:17:37', 'dsad', 'dsadasda'),
-(79, 'user', 'admin', 1, '02/25/2020 17:17', '2020-02-19 14:17:45', 'dsad', 'dsadasda'),
-(80, 'user', 'kullanici1', 1, '02/26/2020 17:21', '2020-02-19 14:22:05', 'ahaaa', 'dsadas');
+INSERT INTO `soru` (`id`, `soran`, `departman`, `alici`, `onem`, `soru_zaman`, `soruldugu_zaman`, `soru_konu`, `soru_detay`, `sonuc`) VALUES
+(92, 'Kadir İnanır', '1', 'Hüseyin Ulan', 2, '02/19/2020 16:18', '2020-02-20 13:19:03', 'konuuu', 'dsada', '0'),
+(93, 'Oğulcan Pabuccu', '2', 'Ahmet Mehmet', 1, '02/27/2020 16:19', '2020-02-20 13:19:53', 'konu2', 'agagagga', '0'),
+(94, 'Kadir İnanır', '1', 'Hüseyin Ulan', 3, '02/27/2020 17:20', '2020-02-20 14:20:52', 'dsadas', 'dsadas', '0'),
+(95, 'Oğulcan Pabuccu', '2', 'Ahmet Mehmet', 1, '02/27/2020 17:33', '2020-02-20 14:33:38', 'olduumu hocam', 'olmuştur be', '1');
 
 --
 -- Dökümü yapılmış tablolar için indeksler
@@ -206,13 +162,13 @@ ALTER TABLE `soru`
 -- Tablo için AUTO_INCREMENT değeri `cevap`
 --
 ALTER TABLE `cevap`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=130;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=134;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `dosyalar`
 --
 ALTER TABLE `dosyalar`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `kullanici`
@@ -224,7 +180,7 @@ ALTER TABLE `kullanici`
 -- Tablo için AUTO_INCREMENT değeri `soru`
 --
 ALTER TABLE `soru`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=96;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
